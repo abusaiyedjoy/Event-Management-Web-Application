ec22818764d69b3c324acb30e58cce51ae9d3d43
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2025-07-01 22:46:17 +0600

    Update frontend link

--- Client/src/hooks/useAxiosPublic.jsx
+++ Client/src/hooks/useAxiosPublic.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://event-management-web-application.vercel.app",
+    baseURL: import.meta.env.VITE_API_URL || "https://event-management-web-application-backend.onrender.com",
 });
 
 export default function useAxiosPublic() {
-  return axiosPublic;
+    return axiosPublic;
 }
